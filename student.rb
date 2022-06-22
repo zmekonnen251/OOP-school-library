0680aa9e34a0334796f0310ec881f9a64c11c7da
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-06-22 17:30:01 +0200

    Create has-many/belogs-to relationship between Classroom and Student classes. -Created the has-many side in the Classroom class -Created the belongs-to side in the Student class

--- student.rb
+++ student.rb
@@ -2,9 +2,12 @@ require_relative 'person'
 
 # Student class extends person class
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, name, _parent, parent_permission, classroom)
     super(age, name, parent_permission)
-    @class_room = classroom
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
