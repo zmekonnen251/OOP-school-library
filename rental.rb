27905a811f2eb2e4b206f487cf0ead9f3239a413
zmekonnen251 <mekonnen.zelalem251@gmail.com>
2022-06-22 17:36:22 +0200

    Created Rental class  -It has instance variable @date and intialized in the constructor  -Getter and setter added using attr_accessor for @date

---
+++ rental.rb
@@ -0,0 +1,14 @@
+class Rental
+  attr_accessor :date
+  attr_reader :book,:person
+
+  def initialize(date,book,person)
+    @date = date
+
+    @person = person
+    person.rents << self
+
+    @book = book
+    book.rents << self
+  end
+end
